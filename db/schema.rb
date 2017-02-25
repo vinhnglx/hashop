24dcf3d284b3015e9f99cf65eed8f9f7265cabdb
Vincent <vinh.nglx@gmail.com>
2017-02-26 01:19:07 +0800

    Add category_id to Product model. Set relation product belongs_to category

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170225165624) do
+ActiveRecord::Schema.define(version: 20170225170603) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "categories", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "products", force: :cascade do |t|
     t.string "name"
@@ -23,6 +29,8 @@ ActiveRecord::Schema.define(version: 20170225165624) do
     t.boolean "sold_out", default: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "category_id"
+    t.index ["category_id"], name: "index_products_on_category_id"
   end
 
 end
--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -2,14 +2,19 @@
 #
 # Table name: products
 #
-#  id         :integer          not null, primary key
-#  name       :string
-#  price      :integer
-#  sale_price :integer
-#  under_sale :boolean
-#  sold_out   :boolean
-#  created_at :datetime         not null
-#  updated_at :datetime         not null
+#  id          :integer          not null, primary key
+#  name        :string
+#  price       :integer
+#  sale_price  :integer
+#  under_sale  :boolean          default(FALSE)
+#  sold_out    :boolean          default(FALSE)
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#  category_id :integer
+#
+# Indexes
+#
+#  index_products_on_category_id  (category_id)
 #
 
 require 'rails_helper'
@@ -19,15 +24,19 @@ RSpec.describe Product, type: :model do
 
   context 'attributes' do
     it 'is valid with valid attributes' do
+      category_id = Category.create!(name: "Classes").id
+      subject.category_id = category_id
       expect(subject).to be_valid
     end
   end
 
   context 'indexes' do
-    it 'has an index on category_id'
+    it 'has an index on category_id' do
+      expect(ActiveRecord::Base.connection.index_exists?(:products, :category_id)).to be_truthy
+    end
   end
 
-  context 'validates' do
+  context 'validations' do
     it 'is invalid without product name' do
       subject.name = nil
       expect(subject).to be_invalid
@@ -37,8 +46,13 @@ RSpec.describe Product, type: :model do
       subject.price = nil
       expect(subject).to be_invalid
     end
+  end
 
-    it 'is invalid without category'
+  context 'associations' do
+    it "belongs to category" do
+      association = described_class.reflect_on_association(:category)
+      expect(association.macro).to eq :belongs_to
+    end
   end
 end
 
