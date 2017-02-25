10d50768f55f828424b18c0de48254aba268edb9
Vincent <vinh.nglx@gmail.com>
2017-02-25 22:24:19 +0800

    re run last time

--- scripts/run-rubocop.sh
+++ scripts/run-rubocop.sh
@@ -1,51 +1,44 @@
 #!/bin/bash
-  # Travis-CI
-  #
-  # git clone --depth=50 \
-  # git://github.com/packsaddle/example-ruby-travis-ci.git \
-  # packsaddle/example-ruby-travis-ci
-  # cd packsaddle/example-ruby-travis-ci
-  # git fetch origin +refs/pull/1/merge:
-  # git checkout -qf FETCH_HEAD
+echo gem install
+gem install --no-document rubocop-select rubocop rubocop-checkstyle_formatter \
+            checkstyle_filter-git saddler saddler-reporter-github \
+            github_status_notifier
 
-  echo gem install
-  gem install --no-document rubocop-select rubocop rubocop-checkstyle_formatter \
-              checkstyle_filter-git saddler saddler-reporter-github \
-              github_status_notifier
+github-status-notifier notify --state pending --context saddler/rubocop
 
-  github-status-notifier notify --state pending --context saddler/rubocop
+echo git diff
+git diff -z --name-only origin/master
 
-  echo git diff
-  git diff -z --name-only origin/master
+echo rubocop-select
+git diff -z --name-only origin/master \
+ | xargs -0 rubocop-select
 
-  echo rubocop-select
-  git diff -z --name-only origin/master \
-   | xargs -0 rubocop-select
+echo rubocop
+git diff -z --name-only origin/master \
+ | xargs -0 rubocop-select \
+ | xargs rubocop \
+     --require rubocop/formatter/checkstyle_formatter \
+     --format RuboCop::Formatter::CheckstyleFormatter
 
-  echo rubocop
-  git diff -z --name-only origin/master \
-   | xargs -0 rubocop-select \
-   | xargs rubocop \
-       --require rubocop/formatter/checkstyle_formatter \
-       --format RuboCop::Formatter::CheckstyleFormatter
+echo checkstyle_filter-git
+git diff -z --name-only origin/master \
+ | xargs -0 rubocop-select \
+ | xargs rubocop \
+     --require rubocop/formatter/checkstyle_formatter \
+     --format RuboCop::Formatter::CheckstyleFormatter \
+ | checkstyle_filter-git diff origin/master
 
-  echo checkstyle_filter-git
-  git diff -z --name-only origin/master \
-   | xargs -0 rubocop-select \
-   | xargs rubocop \
-       --require rubocop/formatter/checkstyle_formatter \
-       --format RuboCop::Formatter::CheckstyleFormatter \
-   | checkstyle_filter-git diff origin/master
+echo saddler
+git diff -z --name-only origin/master \
+ | xargs -0 rubocop-select \
+ | xargs rubocop \
+     --require rubocop/formatter/checkstyle_formatter \
+     --format RuboCop::Formatter::CheckstyleFormatter \
+ | checkstyle_filter-git diff origin/master \
+ | saddler report \
+    --require saddler/reporter/github \
+    --reporter Saddler::Reporter::Github::PullRequestComment
 
-  echo saddler
-  git diff -z --name-only origin/master \
-   | xargs -0 rubocop-select \
-   | xargs rubocop \
-       --require rubocop/formatter/checkstyle_formatter \
-       --format RuboCop::Formatter::CheckstyleFormatter \
-   | checkstyle_filter-git diff origin/master \
-   | saddler report \
-      --require saddler/reporter/github \
-      --reporter Saddler::Reporter::Github::PullRequestComment
+github-status-notifier notify --exit-status $? --context saddler/rubocop
 
-  github-status-notifier notify --exit-status $? --context saddler/rubocop
+exit 0
