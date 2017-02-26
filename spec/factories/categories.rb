1bc3204c2fc6fdbdbcb1ed0b976af72f105a0826
Vincent <vinh.nglx@gmail.com>
2017-02-26 12:45:53 +0800

    Refactor model tests by using FactoryGirl to create sample data

---
+++ spec/factories/categories.rb
@@ -0,0 +1,16 @@
+# == Schema Information
+#
+# Table name: categories
+#
+#  id         :integer          not null, primary key
+#  name       :string
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
+FactoryGirl.define do
+  sequence(:name) { |n| "Cat #{n}" }
+  factory :category do
+    name
+  end
+end
---
+++ spec/factories/products.rb
@@ -0,0 +1,27 @@
+# == Schema Information
+#
+# Table name: products
+#
+#  id          :integer          not null, primary key
+#  name        :string
+#  price       :integer
+#  sale_price  :integer
+#  under_sale  :boolean          default(FALSE)
+#  sold_out    :boolean          default(FALSE)
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#  category_id :integer
+#
+# Indexes
+#
+#  index_products_on_category_id  (category_id)
+#
+
+FactoryGirl.define do
+  factory :product do
+    name "AQUA Contact Lens"
+    price 2244
+
+    category
+  end
+end
