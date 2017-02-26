4ba7fd6919651592436ee3ec9a7f2187984e609c
Vincent <vinh.nglx@gmail.com>
2017-02-26 15:23:35 +0800

    Refactor ProductController#index

--- app/controllers/api/v1/products_controller.rb
+++ app/controllers/api/v1/products_controller.rb
@@ -2,7 +2,7 @@ module Api
   module V1
     class ProductsController < ApplicationController
       def index
-        render json: Product.includes(:category).all, include: :category
+        render json: Product.includes(:category), include: :category
       end
     end
   end
