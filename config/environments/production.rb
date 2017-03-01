d663e887401f5d548fa1d8cae9df632a7cf415d9
Vincent <vinh.nglx@gmail.com>
2017-03-01 13:24:14 +0800

    Caching with memcachier addon

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -43,7 +43,16 @@ Rails.application.configure do
 
   # Use a different cache store in production.
   # config.cache_store = :mem_cache_store
-  config.cache_store = :dalli_store
+  # config.cache_store = :dalli_store
+  config.cache_store = :dalli_store,
+                        (ENV["MEMCACHIER_SERVERS"] || "").split(","),
+                                            {:username => ENV["MEMCACHIER_USERNAME"],
+                       :password => ENV["MEMCACHIER_PASSWORD"],
+                       :failover => true,
+                       :socket_timeout => 1.5,
+                       :socket_failure_delay => 0.2,
+                       :down_retry_delay => 60
+                      }
   config.action_controller.perform_caching = true
 
   # Use a real queuing backend for Active Job (and separate queues per environment)
